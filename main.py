21d72712f2b0171b80ee91f3305a4ef2e16aabad
Tanchikc34 <tanya123full@gmail.com>
2024-02-08 22:28:57 +0700

    1

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for
+from flask import Flask, url_for, request
 
 app = Flask(__name__)
 
@@ -8,11 +8,13 @@ def root():
     return "<h1 style=color:rgb(200,0,0)>Миссия Колонизация Марса</h1>"
 
 
+# Колонизация Марса
 @app.route('/index')
 def index():
     return "<h1 style=color:rgb(0,200,0)>И на Марсе будут яблони цвести!</h1>"
 
 
+# Рекламная кампания
 @app.route('/promotion')
 def promotion():
     promotion_list = ["Человечество вырастает из детства.", "Человечеству мала одна планета.",
@@ -20,6 +22,7 @@ def promotion():
     return '<br>'.join(promotion_list)
 
 
+# Изображение Марса
 @app.route('/image_mars')
 def image_mars():
     return """<!doctype html>
@@ -36,6 +39,7 @@ def image_mars():
                 </html>"""
 
 
+# Реклама с картинкой
 @app.route('/promotion_image')
 def promotion_image():
     return f'''<!doctype html>
@@ -61,6 +65,99 @@ def promotion_image():
                     <p class="bg-info p-2 text-black">Присоединяйся!</p>
                   </body>
                 </html>'''
+
+
+# Отбор астронавтов
+@app.route('/astronaut_selection', methods=['POST', 'GET'])
+def astronaut_selection():
+    if request.method == 'GET':
+        return f'''<!doctype html>
+                        <html lang="en">
+                          <head>
+                            <meta charset="utf-8">
+                            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+                             rel="stylesheet" 
+                             integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" 
+                             crossorigin="anonymous">
+                            <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                            <title>Привет, Яндекс!</title>
+                          </head>
+                          <body>
+                                    <h1 class="title">Анкета претендента</h1>
+                                    <h4 class="title">на участие в миссии</h4>
+                                    <div>
+                                        <form class="login_form" method="post">
+                                            <input type="text" class="form-control margin" placeholder="Введите фамилию" aria-label="Фамилия">
+                                            <input type="text" class="form-control margin" placeholder="Введите имя" aria-label="Имя">
+                                            <input type="email" class="form-control margin" placeholder="Введите адрес почты">
+                                            <div class="form-group margin">
+                                                <label for="classSelect">Какое у вас образование?</label>
+                                                <select class="form-control" id="classSelect" name="class">
+                                                  <option>Начальное общее</option>
+                                                  <option>Основное общее</option>
+                                                  <option>Среднее общее</option>
+                                                  <option>Среднее профессиональное</option>
+                                                  <option>Высшее I степени</option>
+                                                  <option>Высшее II степени</option>
+                                                  <option>Высшее III степени</option>
+                                                </select>
+                                            </div>
+                                            
+                                            <div class="form-group margin">
+                                                <label for="form-check">Какие у Вас есть профессии?</label>
+                                                <div class="form-group row">
+                                                <div>
+                                                <input type="checkbox" class="form-check-input" id="i1">
+                                                <label class="form-check-label" for="i1">Инженер-исследователь</label>
+                                                </div>
+                                                <div>
+                                                <input type="checkbox" class="form-check-input" id="i2">
+                                                <label class="form-check-label" for="i2">Астрогеолог</label>
+                                                </div>
+                                                <div>
+                                                <input type="checkbox" class="form-check-input" id="i3">
+                                                <label class="form-check-label" for="i3">Метеоролог</label>
+                                                </div>
+                                                </div>
+                                            </div>
+                                            
+                                            <div class="form-group margin">
+                                                <label for="form-check">Укажите пол</label>
+                                                <div class="form-check">
+                                                  <input class="form-check-input" type="radio" name="sex" id="male" value="male" checked>
+                                                  <label class="form-check-label" for="male">
+                                                    Мужской
+                                                  </label>
+                                                </div>
+                                                <div class="form-check">
+                                                  <input class="form-check-input" type="radio" name="sex" id="female" value="female">
+                                                  <label class="form-check-label" for="female">
+                                                    Женский
+                                                  </label>
+                                                </div>
+                                            </div>
+                                            <div class="form-group margin">
+                                                <label for="about">Почему Вы хотите принять участие миссии?</label>
+                                                <textarea class="form-control" id="about" rows="3" name="about"></textarea>
+                                            </div>
+                                            <div class="form-group margin">
+                                                <label for="photo">Приложите фотографию</label>
+                                                <input type="file" class="form-control-file" id="photo" name="file">
+                                            </div>
+                                            
+                                            <div class="form-group form-check margin">
+                                                <input type="radio" class="form-check-input" id="acceptRules" name="accept">
+                                                <label class="form-check-label" for="acceptRules">Готовы остаться на Марсе?</label>
+                                            </div>
+                                            
+                                            <button type="submit" class="btn btn-primary">Отправить</button>
+                                        </form>
+                                    </div>
+                                  </body>
+                        </html>'''
+    elif request.method == 'POST':
+        return "Форма отправлена"
 
 
 if __name__ == '__main__':
