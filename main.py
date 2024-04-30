319267c98a732730cfb2794538d565834a5fa3ee
Татьяна Афанасьева <128277569+Tanchikc34@users.noreply.github.com>
2024-04-30 18:19:46 +0700

    Update main.py

--- main.py
+++ main.py
@@ -23,5 +23,11 @@ def training(prof):
     return render_template('training.html', **param)
 
 
+# Тренировки в полёте
+@app.route('/list_prof/<list>')
+def list_prof(list):
+    return render_template('list_prof.html', list=list)
+
+
 if __name__ == '__main__':
     app.run(port=8070, host='127.0.0.1')
